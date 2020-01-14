29f9dad77ad6e1a33a5c09dd5b8fdf4566f3cbd9
Le Lan-Toson <clelantoson@hotmail.fr>
2020-01-14 10:40:57 +0100

    fini exo 12

--- exo_12.rb
+++ exo_12.rb
@@ -1,9 +1,9 @@
-puts "Dis moi un nombre entre 1 et 100"
+puts "Dis moi un nombre entre 0 et 100"
 
 numbers = gets.chomp.to_i
 
 
-    for numbers in 1..numbers do
+    for numbers in 0..numbers do
         puts numbers
 
 end
