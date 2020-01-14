60c83f3f34f6774258ac6943c16a669aa3300736
Le Lan-Toson <clelantoson@hotmail.fr>
2020-01-14 23:03:10 +0100

    maj  ex 15: name variable made clearer

--- exo_15.rb
+++ exo_15.rb
@@ -1,11 +1,10 @@
 puts "Quelle est ton année de naissance ?"
 
-birth_year = gets.chomp.to_i
-birth_year_init = birth_year
+    birth_year = gets.chomp.to_i
+    year = birth_year
 
-    for birth_year in birth_year..2020 do
+    for year in birth_year..2020 do
         puts birth_year
-        age = birth_year - birth_year_init
-        puts "En #{birth_year} tu avais #{age} ans !"
+        age = year - birth_year
+        puts "En #{year} tu avais #{age} ans !"
     end
-
