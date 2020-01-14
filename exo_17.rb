f69d8eb95a6950b247c2ee1ea83b7147da4b3fce
Le Lan-Toson <clelantoson@hotmail.fr>
2020-01-14 12:38:57 +0100

    indentation exo 17

--- exo_17.rb
+++ exo_17.rb
@@ -3,16 +3,16 @@ puts "Quelle est ton année de naissance ?"
 birth_year = gets.chomp.to_i
 birth_year_init = birth_year
 
-    for birth_year in birth_year..2020 do
-        puts birth_year
-        age = birth_year - birth_year_init
-        years = 2020 - birth_year
+for birth_year in birth_year..2020 do
+    puts birth_year
+    age = birth_year - birth_year_init
+    years = 2020 - birth_year
 
-        if years == age
-            puts "Il y a #{years} ans, en #{birth_year} tu avais #{age} ans ! Tu avais la moitié de ton age aujourdh'ui !"
-        
-        else
-            puts "Il y a #{years} ans, en #{birth_year} tu avais #{age} ans !"
+    if years == age
+        puts "Il y a #{years} ans, en #{birth_year} tu avais #{age} ans ! Tu avais la moitié de ton age aujourdh'ui !"
     
-        end
+    else
+        puts "Il y a #{years} ans, en #{birth_year} tu avais #{age} ans !"
+
     end
+end
