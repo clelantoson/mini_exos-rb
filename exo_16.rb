d4e201916a5bf61a9c4c02d42f3a9188447d5c4e
Le Lan-Toson <clelantoson@hotmail.fr>
2020-01-14 12:09:39 +0100

    16 done

---
+++ exo_16.rb
@@ -0,0 +1,13 @@
+puts "Quelle est ton année de naissance ?"
+
+birth_year = gets.chomp.to_i
+birth_year_init = birth_year
+
+    for birth_year in birth_year..2020 do
+        puts birth_year
+        age = birth_year - birth_year_init
+        years = 2020 - birth_year
+        
+        puts "Il y a #{years} ans, en #{birth_year} tu avais #{age} ans !"
+    end
+
